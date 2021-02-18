aa17e8a85f4020df91d07dd92ac3d11de53cdab0
LipzDev <filipe.gt1998@outlook.com>
2021-02-17 23:16:35 -0300

    Arquivos

---
+++ Portfolio v3/js/modules/menu-navigation.js
@@ -0,0 +1,17 @@
+export default function initMenuNavigation(){
+   const menuNavigation = document.querySelectorAll('.menu-navigation button');
+
+   menuNavigation.forEach((item) => {
+      item.addEventListener('click', function(){
+         let menuActive = document.querySelector('.actived');
+
+         if(menuActive.classList.contains('actived')){
+            menuActive.classList.remove('actived');
+         }
+         item.classList.add('actived');
+
+         let sound = document.querySelector('.menu-navigation audio');
+         sound.play();
+      });
+   });
+}
--- Portfolio v3/js/script.js
+++ Portfolio v3/js/script.js
@@ -1,4 +1,5 @@
 import initMenuMobile from './modules/menu-mobile.js';
-
+import initMenuNavigation from './modules/menu-navigation.js';
 
 initMenuMobile();
+initMenuNavigation();
